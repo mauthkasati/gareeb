583937050f3bcc4213471c60284eb2639deca208
Muath <muathkasati@gmail.com>
2023-03-06 17:02:18 +0200

    updating html and css and js files

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  if(document.getElementById("aaa").style.color=="red"){
+    document.getElementById("aaa").style.color="blue";
+  }
+  else{
+    document.getElementById("aaa").style.color="red";
+  }
+}
